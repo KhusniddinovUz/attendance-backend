e4248373e90a1b9142aa3ea5406bfa4faa9e640f
KhusniddinovUz <khusniddinovuz@gmail.com>
2025-05-01 04:38:53 +0200

    return group attendance list when student status updated

--- attendance/views.py
+++ attendance/views.py
@@ -1,4 +1,5 @@
-from rest_framework import generics
+from rest_framework import generics, status
+from rest_framework.response import Response
 from .models import Attendance
 from .serializers import AttendanceSerializer, AttendanceStatusUpdateSerializer
 
@@ -7,7 +8,6 @@ class AttendanceListView(generics.ListAPIView):
     serializer_class = AttendanceSerializer
     queryset = Attendance.objects.all()
     permission_classes = []
-
 
     def get_queryset(self):
         queryset = super().get_queryset()
@@ -47,7 +47,25 @@ class UpdateAttendanceStatusByStudentLesson(generics.UpdateAPIView):
         try:
             attendance = Attendance.objects.get(student_name__name=student_name,
                                                 lesson_name__name=lesson_name,
-                                                lesson_name__para=para,)
+                                                lesson_name__para=para, )
             return attendance
         except Attendance.DoesNotExist:
             raise ValueError("Attendance record not found.")
+
+    def update(self, request, *args, **kwargs):
+        instance = self.get_object()
+        serializer = self.get_serializer(instance, data=request.data, partial=True)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+
+        # ✅ Return all Attendance records instead of just the updated one
+        group_name = self.request.data.get('group_name')
+        lesson_name = self.request.data.get('lesson_name')
+        para = self.request.data.get('para')
+        date = self.request.data.get('date')
+        all_records = Attendance.objects.all()
+        all_records = all_records.filter(lesson_name__name=lesson_name,
+                                         student_name__group_name=group_name, date=date,
+                                         lesson_name__para=para)
+        return Response(AttendanceSerializer(all_records, many=True).data,
+                        status=status.HTTP_200_OK)
