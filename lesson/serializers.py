4a9ab2b0c7934334b7da93cc9ae122ba3331043d
KhusniddinovUz <khusniddinovuz@gmail.com>
2025-05-04 15:47:33 +0200

    Prevent duplicate lessons

--- lesson/serializers.py
+++ lesson/serializers.py
@@ -1,10 +1,18 @@
 from .models import Lesson
 from rest_framework import serializers
+from rest_framework.validators import UniqueTogetherValidator
 
 class LessonSerializer(serializers.ModelSerializer):
     class Meta:
         model = Lesson
-        fields = "__all__"
+        fields = ["id", "group_name", "teacher_name", "para", "date",]
+        validators = [
+            UniqueTogetherValidator(
+                queryset=Lesson.objects.all(),
+                fields=["group_name", "para", "date"],
+                message="Tanlangan dars mavjud",
+            )
+        ]
         extra_kwargs = {
             "teacher": {"read_only": True},
         }
