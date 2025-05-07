8e5987b5d6c7720bbb994ce61edc3431d4aecd5f
KhusniddinovUz <khusniddinovuz@gmail.com>
2025-05-07 13:52:04 +0200

    make attendance status "+" by default

--- attendance/models.py
+++ attendance/models.py
@@ -3,5 +3,5 @@ from django.db import models
 class Attendance(models.Model):
     student_name = models.ForeignKey('student.Student', on_delete=models.CASCADE, related_name='attendances')
     lesson_name = models.ForeignKey('lesson.Lesson', on_delete=models.CASCADE, related_name='attendances')
-    status = models.CharField(max_length=1, default="-")
+    status = models.CharField(max_length=1, default="+")
     date = models.DateField(blank=True, null=True)
