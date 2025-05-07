1859dc75618453026f56f52cba9eec86d5eed8ee
said7pro <murodkhujaevs@gmail.com>
2025-05-07 10:32:38 +0500

    changed to postgre from sql

--- attendance_tracker/settings.py
+++ attendance_tracker/settings.py
@@ -1,5 +1,6 @@
 from pathlib import Path
 from datetime import timedelta
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -87,8 +88,12 @@ WSGI_APPLICATION = 'attendance_tracker.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.environ.get('POSTGRES_DB'),
+        'USER': os.environ.get('POSTGRES_USER'),
+        'PASSWORD': os.environ.get('POSTGRES_PASSWORD'),
+        'HOST': os.environ.get('POSTGRES_SERVER', 'db'),  # 'db' is the service name in docker-compose
+        'PORT': os.environ.get('POSTGRES_PORT', '5432'),
     }
 }
 
