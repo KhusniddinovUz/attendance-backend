3be390decca7caa776b0bdad948474bd468faeb8
KhusniddinovUz <khusniddinovuz@gmail.com>
2025-05-07 22:07:31 +0200

    send teacher_name with attendance for admins

--- attendance/serializers.py
+++ attendance/serializers.py
@@ -4,9 +4,10 @@ from .models import Attendance
 class AttendanceSerializer(serializers.ModelSerializer):
     student_name = serializers.SerializerMethodField()
     lesson_name = serializers.SerializerMethodField()
+    teacher_name = serializers.CharField(source='lesson_name.teacher_name.name')
     class Meta:
         model = Attendance
-        fields = ["student_name", "lesson_name", "status", "date"]
+        fields = ["student_name", "lesson_name", "status", "date", "teacher_name"]
 
     def get_student_name(self, obj):
         return obj.student_name.name
